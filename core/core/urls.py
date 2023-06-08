1412f3c17689218b9552de77a9b93d57497b84ba
sunr4y <sunraydesign18@gmail.com>
2023-06-08 12:10:01 +0200

    Se agregó app Sellers (Solo maquetado)

--- core/core/urls.py
+++ core/core/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("cart/", include("cart.urls")),
     path("orders/", include("orders.urls")),
     path("feedback/", include("feedback.urls")),
+    path("sellers/", include("sellers.urls")),
 ]
